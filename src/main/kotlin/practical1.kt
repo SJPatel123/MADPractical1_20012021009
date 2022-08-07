bc9505947a8f0213e760e72126f6cf0d6669d239
Snej Hirpara <snejhirpara123@gmail.com>
2022-08-07 15:38:58 +0530

    These are Practical-1,2 and 3

--- src/main/kotlin/practical1.kt
+++ src/main/kotlin/practical1.kt
@@ -1,8 +1,20 @@
 fun main(){
-    var x : Int = 0
-    val y : Int = 1
-    var z = 39.5f
-    println("x = $x")
-    println("y = $y")
-    println("z = $z")
+    var i : Int = 40
+    var fl : Float = 12.50f
+    var ch : Char = 'H'
+    var str : String = "Snej Hirpara"
+    var bln : Boolean = true
+    var dbl : Double = 88.50
+    var lng : Long = 354226751
+    var shr : Short = -10
+    var bt : Byte = 127
+    println("Integer Value = $i")
+    println("Float Value = $fl")
+    println("Character Value = $ch")
+    println("String Value = $str")
+    println("Boolean Value = $bln")
+    println("Double Value = $dbl")
+    println("Long Value = $lng")
+    println("Short Value = $shr")
+    println("Byte Value = $bt")
 }
